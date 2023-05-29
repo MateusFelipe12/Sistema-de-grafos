3ce430e2ead884d3523d8de393fc4a43955570f3
MateusFelipe12 <mateus.rauber@unochapeco.edu.br>
2023-05-29 19:09:45 -0300

    Iniciando o dijkstra

--- index.js
+++ index.js
@@ -25,20 +25,20 @@ const clearStorage = (item) => {
     localStorage.removeItem(item)
 }
 
-const getNodesGraph = function(){
+const getNodesGraph = function () {
     getItemStorage('vertex');
     let allNodes = [];
     window['vertex'].forEach((e, i) => {
-        if(e){
+        if (e) {
             e.forEach(element => {
-                if(element ) allNodes.push(window['vertex'][i].indexOf(element)); 
+                if (element) allNodes.push(window['vertex'][i].indexOf(element));
             });
             allNodes.push(window['vertex'].indexOf(e));
         }
     })
     // unifica o array -> passou a ser json
     allNodes = new Set(allNodes)
-    allNodes = Array.from( allNodes).sort((a, b) => a - b);
+    allNodes = Array.from(allNodes).sort((a, b) => a - b);
     return allNodes;
 }
 
@@ -83,27 +83,27 @@ const addVertex = () => {
 
 const listVertex = () => {
     let allNodes = getNodesGraph();
-    if(allNodes.length){
+    if (allNodes.length) {
         let list = $('#list_vertex >div');
 
         let htmlTd = '<th>#</th>';
-        allNodes.forEach((i) => htmlTd+='<th>'+i+'</th>')
+        allNodes.forEach((i) => htmlTd += '<th>' + i + '</th>')
         let htmlTr = `<thead>${htmlTd}</thead>`;
 
         allNodes.forEach((i) => {
             htmlTd = '';
             allNodes.forEach((j) => {
-                if( window['vertex'][i] && window['vertex'][i][j] ){
-                    htmlTd += `<td has>${window['vertex'][i][j]}</td>` 
-                } else if (window['vertex'][j] && window['vertex'][j][i]){
-                    htmlTd += `<td has>${window['vertex'][j][i]}</td>` 
-                } else if(i==j){
+                if (window['vertex'][i] && window['vertex'][i][j]) {
+                    htmlTd += `<td has>${window['vertex'][i][j]}</td>`
+                } else if (window['vertex'][j] && window['vertex'][j][i]) {
+                    htmlTd += `<td has>${window['vertex'][j][i]}</td>`
+                } else if (i == j) {
                     htmlTd += `<td>0</td>`
-                } else htmlTd += `<td>-</td>`
+                } else htmlTd += `<td>X</td>`
             })
             htmlTr += `<tr><th scope="row">${i}</th>${htmlTd}</tr>`
         })
-        
+
         $('#list_vertex >div').html(`
             <div class="text-start">
                 <p>Para remover um vertice, clique duas vezes no vertice</p>
@@ -116,15 +116,15 @@ const listVertex = () => {
             </div>
             <table>${htmlTr}</table>
         `)
-        
-        list.find('td[has]').on('dblclick', (e) =>{
+
+        list.find('td[has]').on('dblclick', (e) => {
             let i = $(e.target).siblings('th').text();
             let j = $(e.target).index();
             j = $(list.find('thead th')[j]).text()
             removeVertex(i, j)
         })
 
-        $('#clear-vertex').click(()=>{
+        $('#clear-vertex').click(() => {
             clearStorage('vertex');
             listVertex()
             insertVertexSelect()
@@ -132,9 +132,9 @@ const listVertex = () => {
     } else $('#list_vertex').html('<div class="text-center" >Adicione um vertice para iniciar</div>')
 }
 
-const removeVertex = function(i,j){
+const removeVertex = function (i, j) {
     getItemStorage('vertex');
-    if( window['vertex'][i] && window['vertex'][i][j] ){
+    if (window['vertex'][i] && window['vertex'][i][j]) {
         delete window['vertex'][i][j];
     } else delete window['vertex'][j][i];
     setItemStorage('vertex');
@@ -186,7 +186,7 @@ const addTrunk = () => {
 
 const listTrunk = () => {
     let list = $('#list_trunk ul');
-    
+
     getItemStorage('trunk');
     list.html(`
     <li class="list-group-item">
@@ -201,8 +201,8 @@ const listTrunk = () => {
     </li>
     `)
     window['trunk'].forEach((e, i) => {
-        if(e)
-        list.append(`
+        if (e)
+            list.append(`
             <li  class="list-group-item">
                 <div class="row">
                 <div class="col-2">${e.capacity}</div>
@@ -218,7 +218,7 @@ const listTrunk = () => {
                 </div>
             </li>
             `)
-        });
+    });
     deleteTrunk();
 }
 
@@ -226,7 +226,7 @@ const deleteTrunk = () => {
     $('[id*="delete-trunk"]').click((e) => {
         let btn = $(e.target).closest('button')
         let index = btn.attr('index');
-        if(index){
+        if (index) {
             getItemStorage('trunk');
             delete window['trunk'][index];
             btn.closest('li').remove();
@@ -246,7 +246,7 @@ const addDelivery = () => {
         let destiny = $(this).find('select[name="destiny"]').val();
         let merchandisesList = $(this).find('#list-merchandise ul li:not(.head)');
 
-        if(!origin || !destiny ) {
+        if (!origin || !destiny) {
             $.toast("Verifique os valores informados");
             return false;
         }
@@ -270,8 +270,8 @@ const addDelivery = () => {
             })
         })
 
-        window['delivery'].push({origin, destiny, merchandises})
-        
+        window['delivery'].push({ origin, destiny, merchandises })
+
         // manda guardar no local storage pra não perder informações
         setItemStorage('delivery');
 
@@ -284,14 +284,14 @@ const addDelivery = () => {
         $.toast("Entrega adicionada");
         // return false faz com que o formulario não siga para o evento
 
-        listDelivery();    
+        listDelivery();
         return false;
     })
 }
 
 const listDelivery = () => {
     let list = $('#list_delivery');
-    
+
     getItemStorage('delivery')
     let html = ''
     let weight = 0;
@@ -299,12 +299,12 @@ const listDelivery = () => {
     let width = 0;
     let depth = 0;
     let volumetry = 0;
-    window['delivery'].forEach((e,i) => {
+    window['delivery'].forEach((e, i) => {
         let htmlDelivery = ''
-        if(e){
-            
+        if (e) {
+
             e['merchandises'].forEach((element, index) => {
-                if(element){
+                if (element) {
                     htmlDelivery += `
                     <div class="row">
                     <div class="col-2">Peso: ${element.weight}</div>
@@ -338,8 +338,8 @@ const listDelivery = () => {
             <div class="col-2">Volume: ${volumetry}</div>
             <div class="col-2"></div>
             </div>`
-            
-            html +=`
+
+            html += `
             <div class="card">
             <div class="card-body">
             <div class="row">
@@ -354,12 +354,12 @@ const listDelivery = () => {
                         </div>
                         </div>
             </div>
-            ` 
+            `
         }
     })
     list.html(html)
     removeDelivery();
-    if(!weight && !height && !width && !depth && !volumetry){
+    if (!weight && !height && !width && !depth && !volumetry) {
         list.html(` Adicione entregas para visualiza-las`)
     }
 }
@@ -372,7 +372,7 @@ const removeDelivery = () => {
         setItemStorage('delivery')
         listDelivery()
     })
-    
+
     $('.remove-delivery').click((e) => {
         getItemStorage('delivery')
         let index = $(e.target).closest('button').attr('index')
@@ -385,11 +385,11 @@ const removeDelivery = () => {
 const insertVertexSelect = () => {
     let allNodes = getNodesGraph();
     let html = '';
-    allNodes.forEach(e => html +=`<option value="${e}">${e}</option>`)
-    
+    allNodes.forEach(e => html += `<option value="${e}">${e}</option>`)
+
     $(`#form_add_delivery select[name="destiny"]`).html(
         '<option value="" disabled selected>Selecione o vértice de destino</option>' + html
-        );
+    );
     $(`#form_add_delivery select[name="origin"]`).html(
         '<option value="" disabled selected>Selecione o vértice de origem</option>' + html
     );
@@ -430,6 +430,95 @@ const deleteMerchandise = () => {
         return false;
     })
 }
+
+function dijkstra(graph, source) {
+    const distances = {};
+    const visited = {};
+    const queue = [];
+
+    const numVertices = graph.length;
+
+    for (let vertex = 0; vertex < numVertices; vertex++) {
+        distances[vertex] = Infinity;
+    }
+
+    distances[source] = 0;
+
+    queue.push({ vertex: source, distance: 0 });
+
+    while (queue.length > 0) {
+        queue.sort((a, b) => a.distance - b.distance);
+        const { vertex, distance } = queue.shift();
+
+        if (visited[vertex]) {
+            continue;
+        }
+
+        visited[vertex] = true;
+
+        for (let neighbor = 0; neighbor < numVertices; neighbor++) {
+            const weight = graph[vertex][neighbor];
+            if (weight > 0) {
+                const newDistance = distance + weight;
+
+                if (newDistance < distances[neighbor]) {
+                    distances[neighbor] = newDistance;
+                    queue.push({ vertex: neighbor, distance: newDistance });
+                }
+            }
+        }
+    }
+
+    return distances;
+}
+
+function dijkstra2(graph, source) {
+    const distances = {};
+    const paths = {};
+    const visited = {};
+    const queue = [];
+    
+    // vai retornar os nodes sem repetir e medir o tamanho correto
+    const numVertices = getNodesGraph().length;
+    // zerando tudo   
+    for (let vertex = 0; vertex < numVertices; vertex++) {
+      distances[vertex] = Infinity;
+      paths[vertex] = "";
+    }
+    
+    // ir do node 0 ao 0 o peso é 0
+    distances[source] = 0;
+    
+    // ja salva na lista
+    queue.push({ vertex: source, distance: 0 });
+  
+    while (queue.length > 0) {
+      queue.sort((a, b) => a.distance - b.distance);
+      const { vertex, distance } = queue.shift();
+        
+      if (visited[vertex]) {
+        continue;
+      }
+  
+      visited[vertex] = true;
+  
+      for (let neighbor = 0; neighbor < numVertices; neighbor++) {
+        const weight = graph[vertex][neighbor];
+        if (weight > 0) {
+            const newDistance = Number(distance) + Number(weight);
+            
+          if (newDistance < distances[neighbor]) {
+            distances[neighbor] = newDistance;
+            paths[neighbor] = paths[vertex] !== "" ? paths[vertex] + "_" + neighbor.toString() : vertex.toString();
+            queue.push({ vertex: neighbor, distance: newDistance });
+          }
+        }
+      }
+    }
+  
+    return [paths,distances];
+  }
+  
 
 const onload = function () {
     addVertex();
